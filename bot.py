d5ba9166602ccb4b4f9e8f69cc36cc510f73e35e
ranabdulrehmann <ranabdulrehmann@gmail.com>
2026-03-02 15:54:06 +0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+import os
+import time
+import ccxt
+
+API_KEY = os.getenv("MEXC_API_KEY", "mx0vglYklOGQGI9761")
+API_SECRET = os.getenv("MEXC_API_SECRET", "529c8bbba99e4b3785d30a8bc5da4594")
+SYMBOL = os.getenv("SYMBOL", "SOL/USDT")
+
+exchange = ccxt.mexc({
+    "apiKey": API_KEY,
+    "secret": API_SECRET,
+    "enableRateLimit": True,
+})
+
+print("Bot started. Reading price...")
+
+while True:
+    try:
+        t = exchange.fetch_ticker(SYMBOL)
+        print("Price:", t["last"])
+        time.sleep(15)
+    except Exception as e:
+        print("Error:", e)
+        time.sleep(30)
